249eeb2e01fb281cfae43bc54b758c95e35f45a0
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2016-11-07 15:29:46 -0500

    add distance calculation

--- load_data.py
+++ load_data.py
@@ -4,6 +4,7 @@ import h5py
 import imread
 from glob import glob
 import numpy as np
+from scipy.ndimage.morphology import distance_transform_edt
 
 raw_files = sorted(glob(os.path.join(sys.argv[1], '*.png')))
 raw = np.stack([imread.imread(im) for im in raw_files], axis=0)
@@ -14,3 +15,8 @@ gt = np.stack([imread.imread(im) for im in gt_files], axis=0)
 f = h5py.File('training_data.h5')
 f.create_dataset('raw', data=raw)
 f.create_dataset('gt', data=gt)
+
+# create distance field (from background, with synapses as background)
+# sampling is 30 nm in Z, 4 in X,Y
+distance = distance_transform_edt(gt == 0, (30, 4, 4))
+f.create_dataset('distance', data=distance)
