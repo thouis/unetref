8f22b10d5b0d9b74b953eac48cd1af05c9c36b89
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2016-12-21 13:29:27 -0500

    loading data is now dataset specific, always includes synapses if available

--- load_synapse_data.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import h5py
-import imread
-from glob import glob
-import numpy as np
-import scipy.ndimage as ndi
-from scipy.ndimage.morphology import distance_transform_edt
-
